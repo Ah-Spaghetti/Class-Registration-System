b52258b649f848e5a824222e61238b96cfd3e1d2
Ah-Spaghetti <126036477+Ah-Spaghetti@users.noreply.github.com>
2023-02-21 19:07:27 -0800

    Delete project_u6i2a-main directory

--- project_u6i2a-main/src/main/model/eventlogging/LogException.java
+++
@@ -1,8 +0,0 @@
-package model.eventlogging;
-
-public class LogException extends Exception {
-
-    public LogException(String msg) {
-        super(msg);
-    }
-}
--- project_u6i2a-main/src/main/model/eventlogging/LogPrinter.java
+++
@@ -1,6 +0,0 @@
-package model.eventlogging;
-
-public interface LogPrinter {
-    //EFFECTS: prints the log
-    void printLog(EventLog el) throws LogException;
-}
